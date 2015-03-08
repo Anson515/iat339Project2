d88d7e63d7c69143aa67c263d1dbded53cc21115
chris <chris@something.com>
2015-03-07 20:46:05 -0800

    url changes and added thank you page

---
+++ templates/js/redirect.js
@@ -0,0 +1,9 @@
+var clickToggle = false;
+$(document).ready(
+	function(){
+		//redirects back to the index page after 5 seconds
+		setTimeout(function(){ 
+			window.location.replace("index.html");
+		}, 5000);
+	}
+);
